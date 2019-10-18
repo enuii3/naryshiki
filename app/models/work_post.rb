cc454fcd064dac769b1d966a48c4cb4b43f5c3aa
nonress <b.nxxxi551@gmail.com>
2019-10-18 20:37:15 +0900

    first push

--- app/models/work_post.rb
+++ app/models/work_post.rb
@@ -4,6 +4,6 @@ class WorkPost < ApplicationRecord
   default_scope -> { order(created_at: :desc)}
   has_many :work_post_category_relations, dependent: :destroy
   has_many :categories, through: :work_post_category_relations
-  validates :categories, presence: true
+  validates :category_ids, presence: true
 
 end
