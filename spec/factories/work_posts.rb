bf3876740ea5d04a2b83bb91573e51c4d38af5d9
nonress <b.nxxxi551@gmail.com>
2019-11-09 22:25:42 +0900

    delete HEAD after git revert from spec/factories/workpost.erb

--- spec/factories/work_posts.rb
+++ spec/factories/work_posts.rb
@@ -1,17 +1,9 @@
 FactoryBot.define do
   factory :work_post do
-<<<<<<< HEAD
-<<<<<<< HEAD
     title {'noname'}
     body {'ruby assosietion'}
     skill {'ruby on rails'}
     reward {20000}
     association :user
-=======
-    
->>>>>>> parent of 0118af8...  change category_ides -> categories
-=======
-    categories { 1 }
->>>>>>> parent of 9a8d83c... generate features user && workpost controller tests
   end
 end
